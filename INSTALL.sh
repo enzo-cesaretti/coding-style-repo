9bb6f10006787fb68ee9a691e5acab2aa49da3cb
Enzo Cesaretti <enzo.cesaretti@epitech.eu>
2023-09-14 14:49:21 +0200

    [UPDATE] INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,14 +5,30 @@ CHMOD=$(which chmod)
 MV=$(which mv)
 RM=$(which rm)
 
+#color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
 # Install the dependencies
+echo -e "${GREEN}Installing dependencies...${RESET}"
 eval "sudo $PIP install -r requirements.txt"
 
 # give permission to execute
+echo -e "${GREEN}Giving permission to execute...${RESET}"
 eval "sudo $CHMOD +x coding-style-repo"
 
+if [ -d "/usr/local/bin/coding-style-repo" ]; then
+    echo -e "${RED}coding-style-repo already installed!${RESET}"
+    echo -e "${RED}Please run REMOVE.sh first!${RESET}"
+    exit 1
+fi
+
 # move to bin
+echo -e "${GREEN}Moving to bin...${RESET}"
 eval "sudo $MV coding-style-repo /usr/local/bin/"
 
-# remove install
-# eval "$RM -r ../coding-style-repo"
+echo -e "${GREEN}Removing files...${RESET}"
+eval "sudo $RM -r ../coding-style-repo"
+
+echo -e "${GREEN}Installed successfully!${RESET}"
